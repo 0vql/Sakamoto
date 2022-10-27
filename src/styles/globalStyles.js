d1f43366b0bec468e66d3c961149c89c9dd4f471
Nyaka <87795454+BrownGrey@users.noreply.github.com>
2022-10-27 13:47:46 +0530

    `Blue Patch` Fix

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -13,6 +13,8 @@ const GlobalStyle = createGlobalStyle`
 			-webkit-user-select: none; /* Safari 3.1+ */
 			-moz-user-select: none; /* Firefox 2+ */
 			-ms-user-select: none; /* IE 10+ */
+                        -webkit-tap-highlight-color: rgba(0,0,0,0);
+                        -webkit-tap-highlight-color: transparent;
 	}
 	body > #root {
 		min-height: 100vh;
